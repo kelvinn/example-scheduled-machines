1eabb5f2e550c8a344e2a3d563c4af44ea69bdc6
Kelvin Nicholson <kelvin@kelvinism.com>
2023-10-28 18:54:20 +1100

    Use 'update' instead of 'run'

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,9 +6,10 @@ MACHINE_ID=$MACHINE_ID  # Initially hard code this to your machine ID for the fi
 APP_NAME="example-scheduled-machines"
 
 fly deploy --ha=false --strategy immediate --wait-timeout 240
-fly scale count 1 --yes --process-group worker
 
-sleep 5 # Wait for machine to get destroyed
+sleep 10 # Wait for deploy to finish
+
+fly scale count 1 --yes --process-group worker
 
 MACHINE_ID=$(fly machine list --json | jq -r -c '.[] | select(.config.metadata.fly_process_group == "worker") | .id')
 STATE=$(fly machine list --json | jq -r -c '.[] | select(.config.metadata.fly_process_group == "worker") | .state')
@@ -20,7 +21,7 @@ until [[ "$STATE" == "stopped" || "$STATE" == "started" ]]
 do
    sleep 10
    STATE=$(fly machine list --json | jq -r -c '.[] | select(.config.metadata.fly_process_group == "worker") | .state')
-   echo $MACHINE_ID, $STATE, "stopped"
+   echo "Waiting for machine" $MACHINE_ID "to enter 'stopped' state. Current state: " $STATE
 done
 
-fly machine run $MACHINE_ID --wait-timeout 600 --restart on-fail --skip-health-checks --schedule=daily --metadata fly_process_group=worker
+fly machine update $MACHINE_ID --yes --wait-timeout 600 --restart on-fail --skip-health-checks --schedule=daily --metadata fly_process_group=worker
